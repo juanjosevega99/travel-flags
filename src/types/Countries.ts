76ebd67fe1e37c70adeb1df387c1b60a073590c8
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-07-10 17:22:14 -0500

    add more types

--- src/types/Countries.ts
+++ src/types/Countries.ts
@@ -4,4 +4,21 @@ export interface Country {
     official: string;
     nativeName: { [key: string]: string };
   };
+  capital: string;
+  population: number;
+  area: number;
+  languages: Language[];
+  currencies: Currency[];
+  // TODO: Add other properties as needed
+}
+
+interface Language {
+  name: string;
+  nativeName: string;
+}
+
+interface Currency {
+  code: string;
+  name: string;
+  symbol: string;
 }
