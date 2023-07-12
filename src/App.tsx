78d01b0f3e231934310da63b862c64c52b26675d
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-07-11 22:32:31 -0500

    use router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import { Header } from './components/Header';
+import { CountryDetails } from './components/allCountries/CountryDetails';
 import { HomePage } from './pages/HomePage';
 
 function App() {
+  const PUBLIC_URL = 'http://localhost:5173'
+
   return (
-    <>
+    <BrowserRouter>
       <Header />
+      <Routes>
+        <Route path='/' caseSensitive element={<HomePage />} />
+        <Route caseSensitive path='/:name' element={<CountryDetails />} />
+      </Routes>
       <HomePage />
-    </>
+    </BrowserRouter>
   );
 }
 
