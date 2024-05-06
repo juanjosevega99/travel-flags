08d5e82de949f9d4a1eee4efe9265956d1d48389
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2024-05-05 21:13:08 -0500

    fix: ts error

--- src/libs/DB.ts
+++ src/libs/DB.ts
@@ -1,6 +1,13 @@
 import { createClient } from '@libsql/client/web';
 
+const dbUrl = process.env.NEXT_PUBLIC_TURSO_DATABASE_URL;
+const authToken = process.env.NEXT_PUBLIC_TURSO_AUTH_TOKEN;
+
+if (!dbUrl || !authToken) {
+  throw new Error('Missing environment configuration.');
+}
+
 export const client = createClient({
-  url: process.env.NEXT_PUBLIC_TURSO_DATABASE_URL,
-  authToken: process.env.NEXT_PUBLIC_TURSO_AUTH_TOKEN,
+  url: dbUrl,
+  authToken: authToken,
 });
