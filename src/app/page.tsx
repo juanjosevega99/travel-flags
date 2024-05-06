327781b4fefaeb19cf9d69d765e575c53356a4ff
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2024-05-05 21:47:32 -0500

    chore: remove Link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import Link from 'next/link';
-
 import ListOfCountries from './countries/page';
 
 export default function Home() {
   return (
     <>
-      <Link href='/'></Link>
       <ListOfCountries />
     </>
   );
