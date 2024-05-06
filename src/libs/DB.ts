1169f496d956b9ae524bdcbda36b8681c900f20d
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2024-05-05 23:01:27 -0500

    refactor

--- src/libs/DB.ts
+++ src/libs/DB.ts
@@ -1,13 +1,12 @@
 import { createClient } from '@libsql/client/web';
 
-const dbUrl = process.env.NEXT_PUBLIC_TURSO_DATABASE_URL;
-const authToken = process.env.NEXT_PUBLIC_TURSO_AUTH_TOKEN;
+import { DB_URL, AUTH_TOKEN } from '../../config';
 
-if (!dbUrl || !authToken) {
+if (!DB_URL || !AUTH_TOKEN) {
   throw new Error('Missing environment configuration.');
 }
 
 export const client = createClient({
-  url: dbUrl,
-  authToken: authToken,
+  url: DB_URL,
+  authToken: AUTH_TOKEN,
 });
