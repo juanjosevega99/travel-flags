f44b46f6f45f1e240db14dc150b829a41ac24b96
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2024-05-05 23:01:14 -0500

    chore: main constants variables

---
+++ config.ts
@@ -0,0 +1,4 @@
+export const API_URL: string = `https://restcountries.com/v3.1`;
+
+export const DB_URL = process.env.NEXT_PUBLIC_TURSO_DATABASE_URL
+export const AUTH_TOKEN = process.env.NEXT_PUBLIC_TURSO_AUTH_TOKEN;
