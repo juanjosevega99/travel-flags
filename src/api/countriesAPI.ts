f746484baef35f1f29198bead1317e4df71c381a
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2024-05-05 21:03:36 -0500

    refactor: apis

--- src/api/countriesAPI.ts
+++ src/api/countriesAPI.ts
@@ -1,12 +1,6 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
 import { Country } from '../types/Countries';
-const url = `https://restcountries.com/v3.1`;
 
-interface ResponseData {
-  success: boolean;
-  message: string;
-}
+const url = `https://restcountries.com/v3.1`;
 
 export async function getAllCountries(): Promise<Country[]> {
   try {
@@ -36,30 +30,16 @@ export async function getCountryByCode(countryCode: string) {
   }
 }
 
-export async function wantToGo(
-  req: NextApiRequest,
-  res: NextApiResponse<ResponseData>
-) {
-  if (req.method === 'POST') {
-    const { userId, countryId } = req.body;
-    // TODO: Add database logic to save the 'have gone' data
-    res.status(200).json({ success: true, message: 'Marked as have gone' });
-  } else {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
-}
-
-export async function haveGone(
-  req: NextApiRequest,
-  res: NextApiResponse<ResponseData>
-) {
-  if (req.method === 'POST') {
-    const { userId, countryId } = req.body;
-    // TODO: Add database logic to save the 'have gone' data
-    res.status(200).json({ success: true, message: 'Marked as have gone' });
-  } else {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+export async function getCountriesByCode(countriesCode: string) {
+  try {
+    const response = await fetch(`${url}/alpha?codes=${countriesCode}`);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch details for ${countriesCode}.`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(`Error while fetching details for ${countriesCode}:`, error);
+    throw error;
   }
 }
