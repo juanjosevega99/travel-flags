b5f9898d6ca1646761e30913e530c47d831cf9c2
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2024-06-07 13:41:04 -0500

    feat: improve login

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { supabase } from '../../../supabaseClient';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+
+  const handleLogin = async () => {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (error) {
+      alert(error.message);
+    } else {
+      router.push('/profile');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <input
+        type='email'
+        placeholder='Email'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type='password'
+        placeholder='Password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
