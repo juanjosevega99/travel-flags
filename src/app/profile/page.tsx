bcf718979b34466e2fd5760eb10052c0e9ba1ed6
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2024-06-07 13:40:20 -0500

    feat: improve profile and fetch countries

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,7 +3,10 @@
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { User } from '@supabase/supabase-js';
 
+import { supabase } from '../../supabaseClient';
 import { client } from '../../libs/DB';
 import { getFirstItem, getCurrencyDisplay } from '../../libs/utils';
 import { Country } from '../../types/Countries';
@@ -19,17 +22,47 @@ const Profile = () => {
   const [haveVisitedCountries, setHaveVisitedCountries] = useState<Country[]>(
     []
   );
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
 
   useEffect(() => {
+    const fetchUser = async () => {
+      const {
+        data: { session },
+        error,
+      } = await supabase.auth.getSession();
+      console.log('🚀 ~ fetchUser ~ session:', session);
+      if (error || !session) {
+        router.push('/auth/login');
+      } else {
+        setUser(session.user);
+      }
+    };
+    fetchUser();
+  }, [router]);
+
+  useEffect(() => {
+    if (!user) return;
+
     const fetchCountryDetails = async (
       sqlQuery: string,
       setter: SetterFunction
     ): Promise<void> => {
+      setLoading(true);
+      setError(null);
       try {
         const response = await client.execute({
           sql: sqlQuery,
-          args: [1], // TODO: Replace '1' with the actual user ID
+          args: [user.id],
         });
+
+        if (!response.rows || response.rows.length === 0) {
+          setter([]);
+          return;
+        }
+
         const countriesCode = response.rows
           .map((row) => row.country_code)
           .join(',');
@@ -39,7 +72,10 @@ const Profile = () => {
         const countriesData: Country[] = await countriesResponse.json();
         setter(countriesData);
       } catch (error) {
-        console.error('Failed to fetch countries to visit:', error);
+        setError('Failed to fetch countries.');
+        setter([]);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -51,7 +87,9 @@ const Profile = () => {
       'SELECT * FROM countries_have_visited WHERE user_id = ?',
       setHaveVisitedCountries
     );
-  }, []);
+  }, [user]);
+
+  if (!user) return <div>Loading...</div>;
 
   return (
     <div>
@@ -78,45 +116,53 @@ const Profile = () => {
         </button>
       </div>
       <div className={styles.container}>
-        {(activeTab === 'wantToGo'
-          ? wantToGoCountries
-          : haveVisitedCountries
-        ).map((country) => (
-          <div key={country.name.official} className={styles.country_card}>
-            <Link
-              href='/countries/[code]'
-              as={`/countries/${country.cca2.toLowerCase()}`}
-            >
-              <div className={styles.country_flag}>
-                <Image
-                  loader={() => country.flags.png}
-                  src={country.flags.png}
-                  alt={`${country.name.common} flag`}
-                  width={270}
-                  height={170}
-                />
-              </div>
-              <div className={styles.country_info}>
-                <h3 className={styles.country_title}>{country.name.common}</h3>
-                <div>
-                  <p>Capital: {getFirstItem(country.capital)}</p>
-                  <p>
-                    Language:{' '}
-                    {country.languages
-                      ? getFirstItem(Object.values(country.languages))
-                      : 'N/A'}
-                  </p>
-                  <p>
-                    Currency:{' '}
-                    {country.currencies
-                      ? getCurrencyDisplay(country.currencies)
-                      : 'N/A'}
-                  </p>
+        {loading ? (
+          <div>Loading countries...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          (activeTab === 'wantToGo'
+            ? wantToGoCountries
+            : haveVisitedCountries
+          ).map((country) => (
+            <div key={country.name.official} className={styles.country_card}>
+              <Link
+                href='/countries/[code]'
+                as={`/countries/${country.cca2.toLowerCase()}`}
+              >
+                <div className={styles.country_flag}>
+                  <Image
+                    loader={() => country.flags.png}
+                    src={country.flags.png}
+                    alt={`${country.name.common} flag`}
+                    width={270}
+                    height={170}
+                  />
                 </div>
-              </div>
-            </Link>
-          </div>
-        ))}
+                <div className={styles.country_info}>
+                  <h3 className={styles.country_title}>
+                    {country.name.common}
+                  </h3>
+                  <div>
+                    <p>Capital: {getFirstItem(country.capital)}</p>
+                    <p>
+                      Language:{' '}
+                      {country.languages
+                        ? getFirstItem(Object.values(country.languages))
+                        : 'N/A'}
+                    </p>
+                    <p>
+                      Currency:{' '}
+                      {country.currencies
+                        ? getCurrencyDisplay(country.currencies)
+                        : 'N/A'}
+                    </p>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
