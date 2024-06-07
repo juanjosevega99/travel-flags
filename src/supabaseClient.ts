67ea13a8bc625ac8ff07b99f9d2bf364c17171a5
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2024-06-07 13:48:09 -0500

    fix: possible undefined

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -5,4 +5,8 @@ import { SUPABASE_ANON_KEY, SUPABASE_URL } from '../config';
 const supabaseUrl = SUPABASE_URL;
 const supabaseKey = SUPABASE_ANON_KEY;
 
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Supabase URL and Key must be provided');
+}
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
